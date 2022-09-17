484f3338e3192140d1aba428c014c1055c90fa50
mustafa <mohameddmusstafa@gmail.com>
2022-09-17 19:57:56 +0700

    Build default layout frame

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import classNames from "classnames/bind";
+import React from "react";
+import styles from "./Header.module.scss";
+
+const cx = classNames.bind(styles);
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}></div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
