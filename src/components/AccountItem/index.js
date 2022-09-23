e9c570975fa14eb0f71d966f05aafb1694f11343
mustafa <mohameddmusstafa@gmail.com>
2022-09-23 21:44:45 +0700

    Build UI section Header section 2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,24 @@
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames/bind";
+import React from "react";
+import styles from "./AccountItem.module.scss";
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+  return (
+    <div className={cx("wrapper")}>
+      <img src="" alt="" className={cx("avatar")} />
+      <div className={cx("info")}>
+        <h4 className={cx("name")}>
+          <span></span>
+          <FontAwesomeIcon className={cx("check")} icon={faCheckCircle} />
+        </h4>
+        <span className={cx("username")}></span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountItem;
